34a6da1897ce86f42061da4e9e6cf1773791847e
Artsem Smalonski <artem.smolonskiy@mail.ru>
2024-05-26 19:07:49 +0200

    replace like to '~*'

--- sql_task.sql
+++ sql_task.sql
@@ -3,9 +3,7 @@ FROM los_angeles_restaurant_health_inspections
 WHERE pe_description = (
     SELECT pe_description
     FROM los_angeles_restaurant_health_inspections
-    WHERE facility_name LIKE '%CAFE%'
-      OR facility_name LIKE '%TEA%'
-      OR facility_name LIKE '%JUICE%'
+    WHERE facility_name ~* 'CAFE|TEA|JUICE'
     GROUP BY pe_description
     ORDER BY COUNT(*) DESC
     LIMIT 1 OFFSET 2
